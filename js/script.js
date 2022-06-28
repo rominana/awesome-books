ac08f01611ef436ed011cdef6c71344ed84af395
Romina <romina.pati@gmail.com>
2022-06-28 13:13:55 -0400

    Add remove button fuction

--- js/script.js
+++ js/script.js
@@ -13,14 +13,14 @@ class Book {
   }
 }
 
-//Create a function to add a new book to the collection
+// Create a function to add a new book to the collection
 function getBookInfo(title, author, id) {
   const divAuthor = document.createElement('div');
   const divTitle = document.createElement('div');
   const removeButton = document.createElement('button');
   const li = document.createElement('li');
 
-  //Add id and classes to the elements
+  // Add id and classes to the elements
   divTitle.classList.add('author');
   divAuthor.classList.add('title');
   removeButton.classList.add('remove');
@@ -34,7 +34,7 @@ function getBookInfo(title, author, id) {
   removeButton.innerText = 'Remove';
   removeButton.type = 'button';
 
-  //append elements
+  // append elements
   li.appendChild(divTitle);
   li.appendChild(divAuthor);
   li.appendChild(removeButton);
@@ -42,12 +42,12 @@ function getBookInfo(title, author, id) {
   return li;
 }
 
-//storage data in the browser
+// storage data in the browser
 function storeData() {
   localStorage.setItem('bookData', JSON.stringify(bookData));
 }
 
-//load data in the browser
+// load data in the browser
 function loadData() {
   const data = localStorage.getItem('bookData');
   if (data) {
@@ -59,3 +59,25 @@ function loadData() {
 }
 
 loadData();
+
+// Add element from list
+
+addBook.addEventListener('click', () => {
+  if (newTitle.value && newAuthor.value) {
+    // create an id to the elements
+    const id = bookData[bookData.length - 1] ? bookData[bookData.length - 1].id + 1 : 1;
+    const book = new Book(newTitle.value, newAuthor.value, id);
+    bookData.push(book);
+    bookList.appendChild(getBookInfo(book.title, book.author, book.id));
+    storeData();
+  }
+});
+
+// remove element from list of books
+function removeListElement(id) {
+  const li = document.getElementById(`book${id}`);
+  bookData = bookData.filter((book) => book.id !== id);
+  storeData();
+}
+
+removeListElement(0);
