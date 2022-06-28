bfe8746f5a9a0b33f72a60733c8ae3931b19c336
Romina <romina.pati@gmail.com>
2022-06-28 12:47:45 -0400

    Create a function to add a new element

--- js/script.js
+++ js/script.js
@@ -1,89 +1,63 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable max-classes-per-file */
 const bookList = document.getElementById('book-list');
 const addBook = document.getElementById('add-book');
 const newTitle = document.getElementById('new-title');
 const newAuthor = document.getElementById('new-author');
 
-const allBooksSection = document.getElementById('all-books');
-const addNewBookSection = document.getElementById('add-new-book');
-const contactSection = document.getElementById('contact');
-const listLink = document.getElementById('list-link');
-const addLink = document.getElementById('add-link');
-const contactLink = document.getElementById('contact-link');
-
-let bookData;
+let bookData = [];
 
 class Book {
-  constructor(title = 'nuevo libro', author = 'Shakespeare', id) {
+  constructor(title = 'Nuevo libro', author = 'Shakespeare', id) {
     this.title = title;
     this.author = author;
     this.id = id;
   }
 }
 
-//Create a collection that keeps a list of books 
+//Create a function to add a new book to the collection
 function getBookInfo(title, author, id) {
-  // create a section dinamically div, li and remove button
-  const div = document.createElement('div');
+  const divAuthor = document.createElement('div');
+  const divTitle = document.createElement('div');
   const removeButton = document.createElement('button');
   const li = document.createElement('li');
 
-  // add classes and atributes to the new created elements
-  div.classList.add('book-info');
+  //Add id and classes to the elements
+  divTitle.classList.add('author');
+  divAuthor.classList.add('title');
   removeButton.classList.add('remove');
-  removeButton.setAttribute('id', `button${id}`); // set atributes line 68
-  removeButton.setAttribute('onclick', `javascript:StorageBooks.removeLiElement(${id})`); // set atributes line 68
+  removeButton.setAttribute('id', `button${id}`);
+  removeButton.setAttribute('onclick', `javascript:removeListElement(${id})`);
   li.classList.add('book');
-  li.setAttribute('id', `book${id}`); // set atributes line 68
+  li.setAttribute('id', `book${id}`);
 
-  div.innerHTML = `${title} by ${author}`;
+  divTitle.innerHTML = title;
+  divAuthor.innerHTML = author;
   removeButton.innerText = 'Remove';
   removeButton.type = 'button';
 
-  li.appendChild(div);
+  //append elements
+  li.appendChild(divTitle);
+  li.appendChild(divAuthor);
   li.appendChild(removeButton);
 
   return li;
 }
 
+//storage data in the browser
+function storeData() {
+  localStorage.setItem('bookData', JSON.stringify(bookData));
+}
 
-class StorageBooks {
-// storage the data in the local store
-  static storeData() {
-    localStorage.setItem('bookData', JSON.stringify(bookData));
-  }
-
-  static loadData() {
-    bookData = [];
-    const data = localStorage.getItem('bookData');
-    if (data) {
-      bookData = JSON.parse(data);
-      bookData.forEach((book) => {
-        bookList.appendChild(getBookInfo(book.title, book.author, book.id));
-      });
-    }
-  }
-
-  // Add books to the list
-  static addLiElement() {
-    if (newTitle.value && newAuthor.value) {
-      const id = bookData[bookData.length - 1] ? bookData[bookData.length - 1].id + 1 : 1;
-      const book = new Book(newTitle.value, newAuthor.value, id);
-      bookData.push(book);
+//load data in the browser
+function loadData() {
+  const data = localStorage.getItem('bookData');
+  if (data) {
+    bookData = JSON.parse(data);
+    bookData.forEach((book) => {
       bookList.appendChild(getBookInfo(book.title, book.author, book.id));
-      StorageBooks.storeData();
-    }
-  }
-
-  // remove element of the list
-  static removeLiElement(id) {
-    const li = document.getElementById(`book${id}`);
-    li.remove();
-    bookData = bookData.filter((book) => book.id !== id); // filter method required
-    StorageBooks.storeData();
+    });
   }
 }
 
-document.addEventListener('DOMContentLoaded', StorageBooks.loadData);
-addBook.addEventListener('click', StorageBooks.addLiElement);
+loadData();
+
+////add element from list
