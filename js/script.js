02ad3d0cb45cbc655f8c189f5e21b1c6cb6309e2
Romina <romina.pati@gmail.com>
2022-06-29 10:18:12 -0400

    Modify from fuctions to classes on script.js file

--- js/script.js
+++ js/script.js
@@ -1,87 +1,78 @@
+/* eslint-disable max-classes-per-file */
 const bookList = document.getElementById('book-list');
 const addBook = document.getElementById('add-book');
 const newTitle = document.getElementById('new-title');
 const newAuthor = document.getElementById('new-author');
 
-let bookData = [];
+const allBooksSection = document.getElementById('all-books');
+const addNewBookSection = document.getElementById('add-new-book');
+const contactSection = document.getElementById('contact');
+const listLink = document.getElementById('list-link');
+const addLink = document.getElementById('add-link');
+const contactLink = document.getElementById('contact-link');
+
+let bookData;
 
 class Book {
-  constructor(title = 'Nuevo libro', author = 'Shakespeare', id) {
+  constructor(title = 'New Book', author = 'John Doe', id) {
     this.title = title;
     this.author = author;
     this.id = id;
   }
 }
 
-// Create a function to add a new book to the collection
 function getBookInfo(title, author, id) {
-  const divAuthor = document.createElement('div');
-  const divTitle = document.createElement('div');
+  const div = document.createElement('div');
   const removeButton = document.createElement('button');
   const li = document.createElement('li');
 
-  // Add id and classes to the elements
-  divTitle.classList.add('author');
-  divAuthor.classList.add('title');
+  div.classList.add('book-info');
   removeButton.classList.add('remove');
   removeButton.setAttribute('id', `button${id}`);
-  removeButton.setAttribute('onclick', `javascript:removeListElement(${id})`);
+  removeButton.setAttribute('onclick', `javascript:StorageBooks.removeListElement(${id})`);
   li.classList.add('book');
   li.setAttribute('id', `book${id}`);
 
-  // Add content to elements
-  divTitle.innerHTML = title;
-  divAuthor.innerHTML = author;
+  div.innerHTML = `${title} by ${author}`;
   removeButton.innerText = 'Remove';
   removeButton.type = 'button';
 
-  // Append elements
-  li.appendChild(divTitle);
-  li.appendChild(divAuthor);
+  li.appendChild(div);
   li.appendChild(removeButton);
 
   return li;
 }
 
-// Storage data in the browser
-function storeData() {
-  localStorage.setItem('bookData', JSON.stringify(bookData));
-}
-
-// Load data in the browser
-function loadData() {
-  const data = localStorage.getItem('bookData');
-  if (data) {
-    bookData = JSON.parse(data);
-    bookData.forEach((book) => {
-      bookList.appendChild(getBookInfo(book.title, book.author, book.id));
-    });
+class StorageBooks {
+  static storeData() {
+    localStorage.setItem('bookData', JSON.stringify(bookData));
   }
-}
 
-loadData();
-
-// Add element from list
-addBook.addEventListener('click', () => {
-  if (newTitle.value && newAuthor.value) {
-    // Create an id to the elements
-    const id = bookData[bookData.length - 1] ? bookData[bookData.length - 1].id + 1 : 1;
-    const book = new Book(newTitle.value, newAuthor.value, id);
-    bookData.push(book);
-    bookList.appendChild(getBookInfo(book.title, book.author, book.id));
-    storeData();
+  static loadData() {
+    bookData = [];
+    const data = localStorage.getItem('bookData');
+    if (data) {
+      bookData = JSON.parse(data);
+      bookData.forEach((book) => {
+        bookList.appendChild(getBookInfo(book.title, book.author, book.id));
+      });
+    }
   }
-});
 
-// Remove element from list of books
-function removeListElement(id) {
-  const li = document.getElementById(`book${id}`);
-  // Remove element from the array
-  bookData = bookData.filter((book) => book.id !== id);
-  // Remove element from the html
-  li.remove();
-  // Store array withot the element on the browser
-  storeData();
-}
+//   static removeListElement(id){
 
-removeListElement(0);
+//     const li = document.getElementById(`book${id}`);
+
+
+//   }
+
+//   static addLiElement(){
+
+//     const id = bookData[bookData.length - 1] ? bookData[bookData.length - 1].id + 1 : 1;
+
+//   }
+
+// }
+
+document.addEventListener('DOMContentLoaded', StorageBooks.loadData);
+// addBook.addEventListener('click', StorageBooks.addLiElement);
